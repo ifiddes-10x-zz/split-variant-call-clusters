4e2ab1268851114d1f6ed70cbbaed3af157f6dd2
Ian Fiddes <ian.fiddes@10xgenomics.com>
2018-04-16 10:13:25 -0700

    Targeted version.

--- stages/call_variants/__init__.py
+++ stages/call_variants/__init__.py
@@ -4,73 +4,49 @@
 #
 import os
 import martian
-import collections
 import subprocess
-import tenkit.bio_io as tk_io
 import tenkit.tabix as tk_tabix
 import cellranger.utils as cr_utils
 
 __MRO__ = '''
 stage CALL_VARIANTS(
-    in bam[] merged_bams,
-    in int[] merged_clusters,
+    in bam[] sorted_bams,
+    in int[] clusters,
     in path reference_path,
-    out vcf variants,
+    in bed target_regions,
+    out vcf.gz variants,
     src py "call_variants",
 ) split using (
-    in string locus,
-    in bam merged_bam,
-    out vcf variant_subset,
+    in bam sorted_bam,
+    in int cluster_id,
+    out vcf.gz variant_subset,
 )
 '''
 
 
 def split(args):
-    # hacky fix to get chrom sizes
-    ref_dict = cr_utils.get_reference_genome_fasta(args.reference_path).replace('.fa', '.dict')
-    assert os.path.exists(ref_dict)
-    lines = [x.split() for x in open(ref_dict)][1:]
-    seqs = [x[1].split(':')[1] for x in lines]
-    sizes = map(int, [x[2].split(':')[1] for x in lines])
-
-    # another hack because of the reference we are using
-    def is_primary(chrom):
-        chrom = chrom.replace('chr', '')
-        return chrom.isdigit() or chrom == 'Y' or chrom == 'X'
-
-    loci = [[chrom, 0, size] for chrom, size in zip(seqs, sizes) if is_primary(chrom)]
-    chunks = []
-    for bam, cluster_id in zip(args.merged_bams, args.merged_clusters):
-        for locus in loci:
-            chunks.append({'locus': locus, 'merged_bam': bam, 'cluster_id': cluster_id,
-                           '__mem_gb': 80, '__threads': 8})
-    return {'chunks': chunks, 'join': {'__mem_gb': 16}}
+    return {'chunks': [{'sorted_bam': bam, 'cluster_id': i, '__threads': 2, '__mem_gb': 2 * 6}
+                       for i, bam in zip(args.clusters, args.sorted_bams)],
+            'join': {'__mem_gb': 16}}
 
 
 def main(args, outs):
     genome_fasta_path = cr_utils.get_reference_genome_fasta(args.reference_path)
-
-    bed_path = martian.make_path('region.bed')
-    with open(bed_path, 'w') as f:
-        f.write('\t'.join(map(str, args.locus)))
+    tmp = martian.make_path('{}.vcf'.format(args.cluster_id))
 
     # Run GATK4
     gatk_args = ['gatk-launch', 'HaplotypeCaller',
                  '-R', genome_fasta_path,
-                 '-I', args.merged_bam,
-                 '-O', outs.variant_subset,
-                 '-L', bed_path,
-                 '--minimum-mapping-quality', '30',
-                 '--min-base-quality-score', '20',
-                 '--dont-use-soft-clipped-bases', 'true',
-                 '--add-output-vcf-command-line', 'false',
+                 '-I', args.sorted_bam,
+                 '-O', tmp,
+                 '-L', args.target_regions,
                  '--native-pair-hmm-threads', str(args.__threads)]
 
     subprocess.check_call(gatk_args)
 
     # fix the header
-    recs = [x for x in open(outs.variant_subset)]
-    with open(outs.variant_subset, 'w') as outf:
+    recs = [x for x in open(tmp)]
+    with open(tmp, 'w') as outf:
         for l in recs:
             if l.startswith('#CHROM'):
                 l = l.split()
@@ -78,26 +54,18 @@ def main(args, outs):
                 l = '\t'.join(l) + '\n'
             outf.write(l)
 
+    tk_tabix.sort_vcf(tmp, outs.variant_subset.replace('.gz', ''))
+    tk_tabix.index_vcf(outs.variant_subset.replace('.gz', ''))
+    os.remove(tmp)
+    os.remove(tmp + '.idx')
+
 def join(args, outs, chunk_defs, chunk_outs):
-    # mapping of cluster ID -> VCFs
-    to_merge = collections.defaultdict(list)
-    for o, d in zip(chunk_outs, chunk_defs):
-        to_merge[d.cluster_id].append(o.variant_subset)
-
-    # merge each VCF subset for a cluster
-    merged_vcfs = []
-    for cluster_id, vcf_list in to_merge.iteritems():
-        merged_vcf = martian.make_path('{}.vcf'.format(cluster_id))
-        tk_io.combine_vcfs(merged_vcf, vcf_list)
-        merged_vcfs.append(merged_vcf + '.gz')
-
     # final merge to make one combined VCF
     tmp = martian.make_path('tmp.vcf')
-    cmd = ['vcf-merge'] + merged_vcfs
+    cmd = ['vcf-merge'] + [chunk.variant_subset for chunk in chunk_outs]
     with open(tmp, 'w') as outf:
         subprocess.check_call(cmd, stdout=outf)
     # Sort and index the files
-    outs.variants = 'variants.vcf.gz'
-    tk_tabix.sort_vcf(tmp, outs.variants)
-    tk_tabix.index_vcf(outs.variants)
+    tk_tabix.sort_vcf(tmp, outs.variants.replace('.gz', ''))
+    tk_tabix.index_vcf(outs.variants.replace('.gz', ''))
     os.remove(tmp)
--- stages/modify_bam/__init__.py
+++ stages/modify_bam/__init__.py
@@ -2,7 +2,9 @@
 #
 # Copyright (c) 2016 10X Genomics, Inc. All rights reserved.
 #
+import martian
 import subprocess
+import tenkit.bam as tk_bam
 import cellranger.utils as cr_utils
 import os
 
@@ -10,33 +12,49 @@ __MRO__ = '''
 stage MODIFY_BAM(
     in bam[] filtered_bams,
     in path reference_path,
-    out bam[] modified_filtered_bams,
+    in int[] clusters,
+    out bam[] sorted_bams,
     src py "modify_bam",
 ) split using(
     in bam filtered_bam,
-    out bam modified_filtered_bam,
+    in int cluster_id,
+    out bam sorted_bam,
 )
 '''
 
 def split(args):
-    return {'chunks': [{'filtered_bam': x, '__mem_gb': 8} for x in args.filtered_bams]}
+    return {'chunks': [{'filtered_bam': bam, 'cluster_id': i, '__mem_gb': 8}
+                       for bam, i in zip(args.filtered_bams, args.clusters)]}
 
 
 def main(args, outs):
     genome_fasta_path = cr_utils.get_reference_genome_fasta(args.reference_path)
 
-
+    tmp_bam = martian.make_path(str(args.cluster_id) + '.unsorted.bam')
     # Correct the STAR mapping from 255 to 60 and take care of split reads
     star_args = ['gatk-launch', 'SplitNCigarReads',
                  '-R', genome_fasta_path,
                  '-I', args.filtered_bam,
-                 '-O', outs.modified_filtered_bam,
+                 '-O', tmp_bam,
                  '--skip-mapping-quality-transform', 'false',
                  '--create-output-bam-index', 'false',
                  '--TMP_DIR', os.getcwd()]
-
     subprocess.check_call(star_args)
+
+    tmp_bam2 = martian.make_path(str(args.cluster_id) + '.fixed.bam')
+    in_bam = tk_bam.create_bam_infile(tmp_bam)
+    out_bam, _ = tk_bam.create_bam_outfile(tmp_bam2, None, None, template=in_bam)
+    for rec in in_bam:
+        rec.is_duplicate = False
+        out_bam.write(rec)
+    out_bam.close()
+
+
+    outs.modified_filtered_bam = martian.make_path('{}.bam'.format(args.cluster_id))
+    subprocess.check_call(['sambamba', 'sort', '-t', str(args.__threads), '-o', outs.sorted_bam, tmp_bam2])
+    os.remove(tmp_bam)
+    os.remove(tmp_bam2)
 
 def join(args, outs, chunk_defs, chunk_outs):
     outs.coerce_strings()
-    outs.modified_filtered_bams = [chunk.modified_filtered_bam for chunk in chunk_outs]
+    outs.sorted_bams = [chunk.sorted_bam for chunk in chunk_outs]
